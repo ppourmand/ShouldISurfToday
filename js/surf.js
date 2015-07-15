405d6847b23e12125c1d788fb35d4dc45d0c6df5
ppourmand <pasha.pourmand@gmail.com>
2015-07-14 21:40:56 -0700

    Update to adhere to AirBnB Javascript style guide

--- js/surf.js
+++ js/surf.js
@@ -1,76 +1,74 @@
 // Basic Information
-APIKEY = '93bc401b3b817c4054c313bc0d601c12';
+var APIKEY = '93bc401b3b817c4054c313bc0d601c12';
 
 // IDs
-veniceID = "2611";
-elportoID = "2677";
+var veniceID = '2611';
+var elportoID = '2677';
 //sunsetblvdID= "3673";
 
 // Queries
-var venice_query = "http://magicseaweed.com/api/" + APIKEY + "/forecast/?spot_id=" + veniceID + "&fields=localTimestamp,condition.temperature,swell.components.combined.height";
-var elporto_query = "http://magicseaweed.com/api/" + APIKEY + "/forecast/?spot_id=" + elportoID + "&fields=localTimestamp,condition.temperature,swell.components.combined.height";
+var veniceQuery = 'http://magicseaweed.com/api/' + APIKEY + '/forecast/?spot_id=' + veniceID + '&fields=localTimestamp,condition.temperature,swell.components.combined.height';
+var elportoQuery = 'http://magicseaweed.com/api/' + APIKEY + '/forecast/?spot_id=' + elportoID + '&fields=localTimestamp,condition.temperature,swell.components.combined.height';
 //var sunsetblvd_query = "http://magicseaweed.com/api/" + APIKEY + "/forecast/?spot_id=" + sunsetblvdID + "&fields=localTimestamp,condition.temperature,swell.components.combined.height";
 
 // Button Click Handlers
 $( window ).load(function() {
-	$.get(elporto_query, function(data){
-	   			var most_recent = data[data.length - 1];   	
-	   			updateInfo(most_recent);
-	   		});	
+	$.get(elportoQuery, function(data){
+    var mostRecent = data[data.length - 1];
+    updateInfo(mostRecent);
+  });
 
-		 $("#elporto").click(function() {
-		 	$(".nav").find(".active").removeClass("active");
-	   		$(this).parent().addClass("active");
+	$('#elporto').click(function() {
+    $('.nav').find('.active').removeClass('active');
+    $(this).parent().addClass('active');
+    $.get(elportoQuery, function(data){
+      var mostRecent = data[data.length - 1];
+      updateInfo(mostRecent);
+    });
+	});
 
-	   		$.get(elporto_query, function(data){
-	   			var most_recent = data[data.length - 1];   	
-	   			updateInfo(most_recent);
-	   		});	
-		});
-
-		$("#venicebeach").click(function() {
-			$(".nav").find(".active").removeClass("active");
-	   		$(this).parent().addClass("active");
-
-	   		$.get(venice_query, function(data){
-	   			var most_recent = data[data.length - 1];   	
-	   			updateInfo(most_recent);
-	   		});	
-		});
+  $('#venicebeach').click(function() {
+		$('.nav').find('.active').removeClass('active');
+    $(this).parent().addClass('active');
+    $.get(veniceQuery, function(data){
+      var mostRecent = data[data.length - 1];
+      updateInfo(mostRecent);
+    });
+	});
 
 	// Edits the Info
 	var updateInfo = function(data){
-		water_temp = data.condition.temperature;
-	   	wave_height = data.swell.components.combined.height;
-	   	wetsuit_thickenss = "";
-	   	$("#p_temp").replaceWith("<p id='p_temp'>" + water_temp + "</p>");
-	   	$("#p_height").replaceWith("<p id='p_height'>" + wave_height + "</p>");
+		var waterTemp = data.condition.temperature;
+    var waveHeight = data.swell.components.combined.height;
+    var wetsuitThickenss = '';
+    $('#p_temp').replaceWith(' <p id=\'p_temp\'>' + waterTemp + '</p>');
+    $('#p_height').replaceWith('<p id=\'p_height\'>' + waveHeight + '</p>');
 
-	   	switch(true){
-			case (water_temp > 72):
-				wetsuit_thickenss = "Rashguard";
-				break;
-	   		case (65 < water_temp < 75):
-	   			wetsuit_thickenss = "1 mm - 2/1 mm";
-	   			break;
-			case (62 < water_temp < 68):
-	   			wetsuit_thickenss = "2 mm - 3/2 mm";
-	   			break;
-			case (58 < water_temp < 63):
-	   			wetsuit_thickenss = "3/2 mm - 4/3 mm";
-	   			break;
-			case (52 < water_temp < 59):
-	   			wetsuit_thickenss = "4/3 mm - 5/4/3 mm";
-	   			break;
-	   		case (43 < water_temp < 53):
-	   			wetsuit_thickenss = "5/4 mm - 5/4/3 mm";
-	   			break;
-	   		case (water_temp < 44):
-	   			wetsuit_thickenss = "6/5 mm - 6/5/4 mm";
-	   			break;
-	   		default:
-	   	}
-		$("#p_suit").replaceWith("<p id='p_suit'>" + wetsuit_thickenss + "</p>");
+    switch(true){
+    
+    case (waterTemp > 72):
+      wetsuitThickenss = 'Rashguard';
+      break;
+    case (65 < waterTemp < 75):
+      wetsuitThickenss = '1 mm - 2/1 mm';
+      break;
+    case (62 < waterTemp < 68):
+      wetsuitThickenss = '2 mm - 3/2 mm';
+      break;
+    case (58 < waterTemp < 63):
+      wetsuitThickenss = '3/2 mm - 4/3 mm';
+      break;
+    case (52 < waterTemp < 59):
+      wetsuitThickenss = '4/3 mm - 5/4/3 mm';
+      break;
+    case (43 < waterTemp < 53):
+      wetsuitThickenss = '5/4 mm - 5/4/3 mm';
+      break;
+    case (waterTemp < 44):
+      wetsuitThickenss = '6/5 mm - 6/5/4 mm';
+      break;
+    default:
+    }
+		$('#p_suit').replaceWith('<p id=\'p_suit\'>' + wetsuitThickenss + '</p>');
 	};
-
 });
